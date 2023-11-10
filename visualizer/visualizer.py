044d527188113a77df82350098bbc22e6da7a94d
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2023-11-10 15:29:55 -0500

    minor

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -14,7 +14,7 @@ disp.start()
 pygame.init()
 
 # Tensor dimensions (N x M)
-N, M = 30, 30  # Example dimensions, modify as needed
+N, M = 10, 10  # Example dimensions, modify as needed
 
 # Window size
 width, height = 600, 600
@@ -31,11 +31,38 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+font = pygame.font.Font(None, 12)
 
 # Example tensors
 obstacle_tensor = np.random.randint(0, 2, (N, M))
 agent_tensor = np.random.randint(0, 5, (N, M))
+goal_tensor = np.zeros((N, M))
 
+# remove agents that are currently on obstacles
+for i in range(N):
+    for j in range(M):
+        if agent_tensor[i, j] > 0 and obstacle_tensor[i, j] > 0:
+            agent_tensor[i, j] = 0
+
+# Assign unique indices to agents
+agent_indices = np.zeros_like(agent_tensor)
+current_index = 1
+for i in range(N):
+    for j in range(M):
+        if agent_tensor[i, j] != 0:
+            agent_indices[i, j] = current_index
+            current_index += 1
+            # now generate a goal for each agent
+            invalid_goal = True
+            counter = 0
+            while invalid_goal and counter < 20:
+                rand_row = np.random.randint(0, N)
+                rand_col = np.random.randint(0, M)
+                if agent_tensor[rand_row, rand_col] == 0 and obstacle_tensor[rand_row, rand_col] == 0:
+                    goal_tensor[rand_row, rand_col] = agent_indices[i, j]
+                    invalid_goal = False
+                counter += 1
+            
 def draw_triangle(center, size, direction, color):
     x, y = center
     if direction == 1:  # Up
@@ -58,10 +85,20 @@ def draw_map():
             if obstacle_tensor[i, j] == 1:
                 pygame.draw.rect(screen, BLACK, rect)
 
-            # Draw agents
+                      # Draw agents and their indices
             if agent_tensor[i, j] != 0:
                 color = [RED, GREEN, BLUE, YELLOW][agent_tensor[i, j] - 1]
                 draw_triangle(rect.center, min(cell_width, cell_height) // 4, agent_tensor[i, j], color)
+                index_text = font.render(str(agent_indices[i, j]), True, BLACK)
+                text_rect = index_text.get_rect(center=rect.center)
+                screen.blit(index_text, text_rect)
+
+            # Draw goals
+            if goal_tensor[i, j] != 0:
+                goal_text = font.render(str(goal_tensor[i, j]), True, BLACK)
+                goal_rect = goal_text.get_rect(center=rect.center)
+                pygame.draw.circle(screen, WHITE, rect.center, min(cell_width, cell_height) // 4)
+                screen.blit(goal_text, goal_rect)
 
 # Video writing setup using OpenCV
 video_filename = 'output.mp4'
