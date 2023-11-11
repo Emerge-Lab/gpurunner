f94cfdab6de0aee26a6b30905054e23e2fb1d378
Aarav Pandya <ap7641@nyu.edu>
2023-11-11 18:20:11 -0500

    Fix rowmajor (hopefully)

--- src/utils.hpp
+++ src/utils.hpp
@@ -5,8 +5,8 @@
 namespace madgrid {
   
 Location fromRowMajor(int32_t linearized, int32_t rowCount, int32_t colCount) {
-  int32_t row = linearized % rowCount;
-  int32_t col = linearized - row * rowCount;
+  int32_t col = linearized % colCount;
+  int32_t row = (int32_t)(linearized / colCount);
 
   return Location { .row = row, .col = col };
 }
